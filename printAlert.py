7f466b530b173a8786832817aa5b19e6ca66ccd6
JihoKim0 <kjhjulie2000@gmail.com>
2022-04-27 00:11:17 +0900

    modify tensorflow error, delete webcam window

--- printAlert.py
+++ printAlert.py
@@ -5,8 +5,11 @@ from imutils import face_utils
 from tensorflow.keras.models import load_model
 import time
 import winsound as sd
+import os
 
 IMG_SIZE = (34, 26)
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
@@ -47,59 +50,53 @@ def beepsound():
     du = 200 #duration, 100ms==1second
     sd.Beep(fr, du)
 
-# main
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-while cap.isOpened():
-  ret, img_cam = cap.read()
+def main(sign):
 
-  if not ret:
-    break
+  while sign == 1:
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    while cap.isOpened():
+      ret, img_cam = cap.read()
+
+    if not ret:
+      break
   
-  #웹캠창 크기
-  img_cam = cv2.resize(img_cam, dsize=(0, 0), fx=0.8, fy=0.8)
+    #웹캠창 크기
+    img_cam = cv2.resize(img_cam, dsize=(0, 0), fx=0.8, fy=0.8)
 
-  img = img_cam.copy()
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = img_cam.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-  faces = detector(gray)
+    faces = detector(gray)
 
-  for face in faces:
-    shapes = predictor(gray, face)
-    shapes = face_utils.shape_to_np(shapes)
+    for face in faces:
+      shapes = predictor(gray, face)
+      shapes = face_utils.shape_to_np(shapes)
 
-    eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
-    eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
+      eye_img_l, eye_rect_l = crop_eye(gray, eye_points=shapes[36:42])
+      eye_img_r, eye_rect_r = crop_eye(gray, eye_points=shapes[42:48])
 
-    eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
-    eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
-    eye_img_r = cv2.flip(eye_img_r, flipCode=1)
+      eye_img_l = cv2.resize(eye_img_l, dsize=IMG_SIZE)
+      eye_img_r = cv2.resize(eye_img_r, dsize=IMG_SIZE)
+      eye_img_r = cv2.flip(eye_img_r, flipCode=1)
 
-    eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
-    eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+      eye_input_l = eye_img_l.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
+      eye_input_r = eye_img_r.copy().reshape((1, IMG_SIZE[1], IMG_SIZE[0], 1)).astype(np.float32) / 255.
 
-    pred_l = model.predict(eye_input_l)
-    pred_r = model.predict(eye_input_r)
+      pred_l = model.predict(eye_input_l)
+      pred_r = model.predict(eye_input_r)
 
-    # visualize
-    state_l = 'O %.1f' if pred_l > 0.1 else '- %.1f'
-    state_r = 'O %.1f' if pred_r > 0.1 else '- %.1f'
+      # visualize
+      state_l = 'O %.1f' if pred_l > 0.1 else '- %.1f'
+      state_r = 'O %.1f' if pred_r > 0.1 else '- %.1f'
 
-    state_l = state_l % pred_l
-    state_r = state_r % pred_r
+      state_l = state_l % pred_l
+      state_r = state_r % pred_r
     
     
-    #pred_l && pred_r < 0.5 -> return Blink alert sign
-    if pred_l < 0.3 and pred_r < 0.3:
+      #pred_l && pred_r < 0.5 -> return Blink alert sign
+      if pred_l < 0.3 and pred_r < 0.3:
         alert.start = time.time()
 
-    if (time.time() - alert.start) > alert.time:
+      if (time.time() - alert.start) > alert.time:
         beepsound()
         alert.start = time.time()
-
-
-    cv2.putText(img, state_l, tuple(eye_rect_l[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
-    cv2.putText(img, state_r, tuple(eye_rect_r[0:2]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
-
-  cv2.imshow('result', img)
-  if cv2.waitKey(1) & 0xFF == 27:
-    break
